25c7c3ae07b9dcdd0d233395d0feaa18a3c79dd6
Adebimpe96 <sannidebimpe30@gmail.com>
2022-09-20 13:45:08 +0100

    Add files via upload
    My portfolio

---
+++ PORTFOLIO/port.js
@@ -0,0 +1,11 @@
+// let tablinks = document.getElementsByClassName("sub-titles");
+// let tabcontents = document.getElementsByClassName("sub-contents");
+//  openTabs =(tabname)=>{
+//     for (tablink of tablinks){
+//         tablink.classList.remove("active-link");
+//     }
+//     for (tabcontent of tabcontents){
+//         tabcontent.classList.remove("active-tab");
+//     }event.currentTarget.classList.add("active-link")
+
+// }
